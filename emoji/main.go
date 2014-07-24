1c0ca7d1381f74dac26d0a5bdc7b7233ba840b32
Arne Roomann-Kurrik <kurrik@twitter.com>
2014-07-24 16:11:47 -0700

    Update the emoji sample to print all the emoji tweets.

--- emoji/main.go
+++ emoji/main.go
@@ -40,34 +40,59 @@ func LoadCredentials() (client *twittergo.Client, err error) {
 	return
 }
 
+func GetTweet(client *twittergo.Client, id string) (tweet *twittergo.Tweet, err error) {
+	var (
+		query = url.Values{"id": []string{id}}
+		url   = fmt.Sprintf("/1.1/statuses/show.json?%v", query.Encode())
+		req   *http.Request
+		resp  *twittergo.APIResponse
+	)
+	if req, err = http.NewRequest("GET", url, nil); err != nil {
+		err = fmt.Errorf("Could not parse request: %v\n", err)
+		return
+	}
+	if resp, err = client.SendRequest(req); err != nil {
+		err = fmt.Errorf("Could not send request: %v\n", err)
+		return
+	}
+	tweet = &twittergo.Tweet{}
+	if err = resp.Parse(tweet); err != nil {
+		err = fmt.Errorf("There was an error: %v\n", err)
+	}
+	return
+}
+
 func main() {
 	var (
 		err    error
 		client *twittergo.Client
-		req    *http.Request
-		resp   *twittergo.APIResponse
 		tweet  *twittergo.Tweet
 	)
 	if client, err = LoadCredentials(); err != nil {
 		fmt.Printf("Could not parse CREDENTIALS file: %v\n", err)
 		os.Exit(1)
 	}
-	query := url.Values{
-		"id": []string{"451453919017697280"},
+	tweet_ids := []string{
+		"451453919017697280",
+		"451453883575848960",
+		"451453847622262784",
+		"451453811035357184",
+		"451453775882899456",
+		"451453738603909120",
+		"451453703375953920",
+		"451453667409817600",
+		"451453613231964160",
+		"451453567233040384",
+		"451453517819949056",
+		"451453478502539264",
+		"451453436769218560",
 	}
-	url := fmt.Sprintf("/1.1/statuses/show.json?%v", query.Encode())
-	if req, err = http.NewRequest("GET", url, nil); err != nil {
-		fmt.Printf("Could not parse request: %v\n", err)
-		os.Exit(1)
+
+	for _, id := range tweet_ids {
+		if tweet, err = GetTweet(client, id); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println(tweet.Text())
 	}
-	if resp, err = client.SendRequest(req); err != nil {
-		fmt.Printf("Could not send request: %v\n", err)
-		os.Exit(1)
-	}
-	tweet = &twittergo.Tweet{}
-	if err = resp.Parse(tweet); err != nil {
-		fmt.Printf("There was an error: %v\n", err)
-		os.Exit(1)
-	}
-	fmt.Println(tweet.Text())
 }
